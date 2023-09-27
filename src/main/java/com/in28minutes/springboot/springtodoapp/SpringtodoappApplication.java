25b3b6db2581de52cf4624f2c2dc4cbe731277b3
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2023-09-27 16:58:20 +0200

    Spring MVC Controller @ResponseBody @Controller

--- src/main/java/com/in28minutes/springboot/springtodoapp/SpringtodoappApplication.java
+++ src/main/java/com/in28minutes/springboot/springtodoapp/SpringtodoappApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringtodoappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringtodoappApplication.class, args);
 	}
