c0d6bd78a7b137374de0423e3ec6f86bd99a4ea8
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2023-09-27 14:22:29 +0200

    spring todo app

---
+++ src/test/java/com/in28minutes/springboot/springtodoapp/SpringtodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.springtodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
