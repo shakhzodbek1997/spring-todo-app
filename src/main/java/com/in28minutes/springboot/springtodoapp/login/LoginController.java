c9f569ab9ccb7b443b70aa487f2fa9873e03b001
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2023-09-28 23:21:51 +0200

    LoginController and login View

---
+++ src/main/java/com/in28minutes/springboot/springtodoapp/login/LoginController.java
@@ -0,0 +1,12 @@
+package com.in28minutes.springboot.springtodoapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @RequestMapping("login")
+    public String goToLoginPage(){
+        return "loginPage";
+    }
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/loginPage.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>LOGIN page</title>
+    </head>
+    <body>
+        Login page Body!
+    </body>
+</html>
