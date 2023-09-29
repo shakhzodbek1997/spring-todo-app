a87ad6536a260a5224cfe22e82adee35e67246a0
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2023-09-29 13:53:34 +0200

    Displaying Login Credentials in a JSP using Model

--- src/main/java/com/in28minutes/springboot/springtodoapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/springtodoapp/login/LoginController.java
@@ -1,13 +1,27 @@
 package com.in28minutes.springboot.springtodoapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class LoginController {
-    @RequestMapping("login")
+    @RequestMapping(value="login", method = RequestMethod.GET)
     public String goToLoginPage(){
         return "loginPage";
+    }
+    @RequestMapping(value="login", method = RequestMethod.POST)
+    public String goToWelcomePage(
+            @RequestParam String name,
+            @RequestParam String password,
+            ModelMap model
+    ){
+        model.put("name", name);
+        model.put("password", password);
+        return "welcome";
+
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>WELCOME page</title>
+    </head>
+    <body>
+        <h2>Welcome to in28minutes!</h2>
+        <div>Your name is ${name}</div>
+        <div> Your Password is ${password}</div>
+
+    </body>
+</html>
