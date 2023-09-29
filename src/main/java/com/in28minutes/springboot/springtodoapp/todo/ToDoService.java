2a65334e03d4653e8d5812b0237b09425e101c0f
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2023-09-29 15:57:59 +0200

    Created Todo and ToDoService

---
+++ src/main/java/com/in28minutes/springboot/springtodoapp/todo/ToDoService.java
@@ -0,0 +1,16 @@
+package com.in28minutes.springboot.springtodoapp.todo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class ToDoService {
+    private static List<Todo> todos;
+    static {
+        todos.add(new Todo(1, "in28minutes", "Learn AWS", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(2, "in28minutes", "Learn DevOps", LocalDate.now().plusYears(2), false));
+        todos.add(new Todo(1, "in28minutes", "Learn Full Stack Development", LocalDate.now().plusYears(3), false));
+    }
+    public List<Todo> findByUsername(String username){
+        return todos;
+    }
+}
