249366d8e71c853eac297ae34e24324ec5d89b50
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2023-09-29 12:18:05 +0200

    Importance of Logging with Spring Boot

--- src/main/java/com/in28minutes/springboot/springtodoapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/springtodoapp/login/LoginController.java
@@ -1,18 +1,19 @@
 package com.in28minutes.springboot.springtodoapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class LoginController {
-
-    // http://localhost:8080/login?name=satish
+    private Logger logger = LoggerFactory.getLogger(getClass());
     @RequestMapping("login")
     public String goToLoginPage(@RequestParam String name, ModelMap model){
         model.put("name", name);
-        System.out.println("Request Param is: " + name); // Just testing Purpose
         return "loginPage";
     }
 }
