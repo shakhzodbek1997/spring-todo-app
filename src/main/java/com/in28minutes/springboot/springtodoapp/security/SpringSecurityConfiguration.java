135760a429763c6fe8a9d287c3eed993b425b574
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2023-10-07 18:11:47 +0200

    configured Spring security to get H2 console Working

--- src/main/java/com/in28minutes/springboot/springtodoapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/springtodoapp/security/SpringSecurityConfiguration.java
@@ -1,13 +1,21 @@
 package com.in28minutes.springboot.springtodoapp.security;
 
+import java.security.PublicKey;
+import java.security.Security;
 import java.util.function.Function;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import jakarta.servlet.http.PushBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -35,5 +43,17 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
     }
 }
